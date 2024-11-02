a8760b887a7aaf61215ea4f88518f165a5bd1f8a
Sam Mathew <76838983+S-legend345@users.noreply.github.com>
2024-11-02 18:38:11 +0530

    Add files via upload

--- sets.py
+++ sets.py
@@ -1,17 +1,18 @@
-# a = eval(input("Enter a set: "))
-# b = eval(input("Enter another set: "))
-# count = 0
+a = eval(input("Enter a set: "))
+b = eval(input("Enter another set: "))
+count = 0
 
 
-# for i in b:
-#     if i in a:
-#         count += 1
+for i in b:
+    if i in a:
+        count += 1
 
-# if count == len(b):
-#     print(b, 'is  subset of', a)
+if count == len(b):
+    print(b, 'is  subset of', a)
 
 
 #Code for printing all subsets
+
 
 def subsets(s):
     subsets = [[]]
@@ -20,5 +21,32 @@ def subsets(s):
     return subsets
 
 #Example Usage
-input_set = [1,2,3]
+input_set = list(input("Enter set elements (in plain form): "))
 print("All subsets: ", subsets(input_set))
+
+
+#FlipBits
+
+def flips(n1,n2):
+    flip = 0
+    while (n1>0 or n2>0):
+        t1 = n1 & 1
+        t2 = n2 & 2
+        if t1 != t2:
+            flip += 1
+        n1 >>= 1
+        n2 >>= 1
+        return flip
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter another number: "))
+print("Number of flips needed: ", flips(n1,n2))
+
+
+#ACP - Substrings
+
+s = input("Enter a string: ")
+ss = [[]]
+for i in s:
+    ss += [current + [i] for current in ss]
+print(ss, 'is the list of subsets')
